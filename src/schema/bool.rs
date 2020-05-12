772d5b30d373543088ad203fa729b80d1e8f714b
Scott Teal <scott@teals.org>
2020-05-12 15:41:04 -0400

    Adding tests to bool validator

--- src/schema/bool.rs
+++ src/schema/bool.rs
@@ -1,6 +1,5 @@
 use std::io;
-use std::io::Error;
-use std::io::ErrorKind::InvalidData;
+use std::io::{Error, ErrorKind::InvalidData};
 use decode::*;
 use super::Validator;
 
@@ -38,8 +37,19 @@ impl ValidBool {
                 Ok(true)
             },
             "nin" => {
-                self.constant = Some(!(read_bool(raw)?));
-                Ok(true)
+                let constant = !(read_bool(raw)?);
+                if let Some(prev) = self.constant {
+                    if prev != constant {
+                        Ok(false)
+                    }
+                    else {
+                        Ok(true)
+                    }
+                }
+                else {
+                    self.constant = Some(constant);
+                    Ok(true)
+                }
             }
             "query" => {
                 self.query = read_bool(raw)?;
@@ -104,3 +114,145 @@ impl ValidBool {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use marker::MarkerType;
+    use encode;
+    use value::Value;
+    use super::*;
+
+    fn read_it(raw: &mut &[u8], is_query: bool) -> io::Result<ValidBool> {
+        if let MarkerType::Object(len) = read_marker(raw)? {
+            let mut validator = ValidBool::new(is_query);
+            object_iterate(raw, len, |field, raw| {
+                if !validator.update(field, raw)? {
+                    Err(Error::new(InvalidData, "Wasn't a valid bool validator"))
+                }
+                else {
+                    Ok(())
+                }
+            })?;
+            validator.finalize(); // Don't care about if the validator can pass values or not
+            Ok(validator)
+
+        }
+        else {
+            Err(Error::new(InvalidData, "Not an object"))
+        }
+    }
+
+    fn validate_bool(v: bool, validator: &ValidBool) -> io::Result<()> {
+        let mut val = Vec::with_capacity(1);
+        encode::write_value(&mut val, &Value::from(v));
+        validator.validate("", &mut &val[..])
+    }
+
+    #[test]
+    fn validate() {
+        let mut test1 = Vec::new();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "query": true,
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_ok());
+        assert!(validate_bool(true,  &validator).is_ok());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": true
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_err());
+        assert!(validate_bool(true,  &validator).is_ok());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": false
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_ok());
+        assert!(validate_bool(true,  &validator).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "nin": true
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_ok());
+        assert!(validate_bool(true,  &validator).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "nin": false
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_err());
+        assert!(validate_bool(true,  &validator).is_ok());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": false,
+            "nin": true,
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_ok());
+        assert!(validate_bool(true,  &validator).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": true,
+            "nin": false,
+        }));
+        let validator = read_it(&mut &test1[..], false).unwrap();
+        assert!(validate_bool(false, &validator).is_err());
+        assert!(validate_bool(true,  &validator).is_ok());
+    }
+
+    #[test]
+    fn bad_validators() {
+        let mut test1 = Vec::new();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "query": 0,
+        }));
+        assert!(read_it(&mut &test1[..], false).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": 0,
+        }));
+        assert!(read_it(&mut &test1[..], false).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "nin": 0,
+        }));
+        assert!(read_it(&mut &test1[..], false).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": true,
+            "nin": true,
+        }));
+        assert!(read_it(&mut &test1[..], false).is_err());
+
+        test1.clear();
+        encode::write_value(&mut test1, &fogpack!({
+            "type": "Bool",
+            "in": false,
+            "nin": false,
+        }));
+        assert!(read_it(&mut &test1[..], false).is_err());
+    }
+
+}

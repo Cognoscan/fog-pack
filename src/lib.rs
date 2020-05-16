4f963a2935445c05163ab6fdd632cd2ab9ec9eb1
Scott Teal <scott@teals.org>
2020-05-16 02:15:11 -0400

    Updating documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,68 @@
-//! fog-pack builds on msgpack with a set of extensions useful for all structured 
-//! data. The ultimate goal is to provide a single coherent approach to encoding 
-//! data, validating it, and compressing it. Any existing data format should be 
-//! replaceable with fog-pack, without compromise.
-//! 
-//! To meet this lofty goal, it extends msg-pack by providing:
-//! 
-//! - A canonical form for all data. Given a known input, the same fog-pack value 
-//! 	will always be generated
-//! - Cryptographic hashes are a value type, and the hash of a fog-pack value can 
-//! 	be calculated
-//! - Encrypted data is a value type, which may contain arbitrary data, a secret 
-//! 	key, or a private key
-//! - Support for symmetric-key cryptography
-//! 	- Data can be encrypted using a secret key
-//! 	- Secret keys may be passed around in encrypted form
-//! - Support for public-key cryptography.
-//! 	- Public keys are a value type
-//! 	- Data can be signed with a secret key
-//! 	- Data can be encrypted with a public key
-//! 	- Private keys may be passed around in encrypted form
-//! - A schema format, allowing for validation of fog-pack values
-//! 	- Specifies subsets of possible values
-//! 	- Schema may be used to filter fog-pack values, allowing them to be used as a 
-//! 		query against a database of values
-//! 	- Schema are themselves fog-pack objects
-//! - Immutable Documents, consisting of a fog-pack object with an optional schema 
-//! 	reference.
-//! - Entries, consisting of a fog-pack object, a key string, and the hash of a 
-//! 	fog-pack Document. These may be used to form mutable links between documents.
-//! - Support for compression. A document or entry may be compressed after encoding 
-//! 	& hashing. Dictionary compression of values is supported if a schema is used.
+/*!
+fog-pack builds on msgpack with a set of extensions useful for all 
+structured data. The ultimate goal is to provide a single coherent approach 
+to encoding data, validating it, and compressing it. Any existing data 
+format can be replaced with fog-pack, without compromise.
+
+To meet this lofty goal, it builds on msgpack by providing:
+
+- A canonical form for all data. Given a known input, the same fog-pack 
+    value will always be generated.
+- Cryptographic hashes are a value type, and the hash of a fog-pack value 
+    can be calculated
+- Encrypted data is a value type, which may contain arbitrary data, a secret 
+    key, or a private key
+- Support for symmetric-key cryptography
+    - Data can be encrypted using a secret key
+    - Secret keys may be passed around in encrypted form
+- Support for public-key cryptography.
+    - Public keys are a value type
+    - Data can be signed with a secret key
+    - Data can be encrypted with a public key
+    - Private keys may be passed around in encrypted form
+- A schema format, allowing for validation of fog-pack values
+    - Specifies subsets of possible values
+    - Schema may be used to filter fog-pack values, allowing them to be used 
+        as a query against a database of values
+    - Schema are themselves fog-pack objects
+- Immutable Documents, consisting of a fog-pack object with an optional 
+    schema reference, and identifiable by their cryptographic Hash.
+- Entries, consisting of a fog-pack object, a key string, and the hash of a 
+    fog-pack Document. These may be used to form mutable links between 
+    documents.
+- Support for compression. A document or entry may be compressed after 
+    encoding & hashing. Dictionary compression of values is supported if a 
+    schema is used, allowing for exceptionally fast compression and high ratios. See 
+    [`zstd`](https://facebook.github.io/zstd/) for more information on the compression used.
+
+# Documents & Entries
+
+fog-pack defines two core ways of working with encoded values: a 
+[`Document`](./struct.Document.html) and an [`Entry`](./struct.Entry.html). Documents contain a 
+single encoded Object, and may have an optional associated Schema (see below). Entries have an 
+associated parent Document and string, and may contain any type of encoded value. Together, these 
+allow for the description of immutable data with small mutable data linking them together. In general:
+
+- Documents contain structured immutable data, like files, content, data records, etc.
+- Entries contain small mutable data, like links to Documents, temporary data, small status 
+    updates, or small ephemeral records.
+
+# Schema
+
+A [`Schema`](./struct.Schema.html) is a special type of Document that describes the format of 
+other documents. It contains additional specifications for what Entries may be attached to a 
+Document, and can include recommmended compression settings, even including a compression 
+dictionary for zstd. Documents that use a specific schema reference it by placing the schema's hash 
+in the value associated with the empty string field, like so:
+
+```json
+{
+    "": "<Hash(Schema Used)>",
+    "text": "Example document"
+}
+```
+
+*/
 
 #![allow(dead_code)]
 #![recursion_limit="500"]
--- src/schema.rs
+++ src/schema.rs
@@ -11,11 +11,194 @@ use validator::{ValidObj, Validator, ValidatorChecklist};
 use Hash;
 use Document;
 
+/**
+*/
+pub struct Schema {
+    hash: Hash,
+    object: ValidObj,
+    entries: Vec<(String, usize)>,
+    types: Vec<Validator>,
+    compressor: zstd_safe::CCtx<'static>,
+    decompressor: zstd_safe::DCtx<'static>,
+    doc_compress: Compression,
+    entry_compress: Vec<(String, Compression)>,
+}
+
+impl Schema {
+    pub fn from_raw(raw: &mut &[u8]) -> io::Result<Schema> {
+        let raw_for_hash: &[u8] = raw;
+        let mut entries = Vec::new();
+        let mut types = Vec::with_capacity(2);
+        let mut type_names = HashMap::new();
+        let mut object = ValidObj::new(true); // Documents can always be queried, hence "true"
+        let mut doc_compress = Default::default();
+        let mut entry_compress = Vec::new();
+        types.push(Validator::Invalid);
+        types.push(Validator::Valid);
+
+        let num_fields = match read_marker(raw)? {
+            MarkerType::Object(len) => len,
+            _ => return Err(Error::new(InvalidData, "Schema wasn't an object")),
+        };
+        object_iterate(raw, num_fields, |field, raw| {
+            match field {
+                "" => {
+                    read_hash(raw).map_err(|_e| Error::new(InvalidData, "Schema's empty field didn't contain root Schema Hash"))?;
+                },
+                "doc_compress" => {
+                    doc_compress = Compression::read_raw(raw)?;
+                }
+                "description" => {
+                    read_str(raw).map_err(|_e| Error::new(InvalidData, "`description` field didn't contain string"))?;
+                },
+                "name" => {
+                    read_str(raw).map_err(|_e| Error::new(InvalidData, "`name` field didn't contain string"))?;
+                },
+                "version" => {
+                    read_integer(raw).map_err(|_e| Error::new(InvalidData, "`name` field didn't contain integer"))?;
+                },
+                "entries" => {
+                    if let MarkerType::Object(len) = read_marker(raw)? {
+                        object_iterate(raw, len, |field, raw| {
+                            let v = Validator::read_validator(raw, false, &mut types, &mut type_names)?;
+                            entries.push((field.to_string(), v));
+                            Ok(())
+                        })?;
+                    }
+                    else {
+                        return Err(Error::new(InvalidData, "`entries` field doesn't contain an Object"));
+                    }
+                },
+                "entries_compress" => {
+                    if let MarkerType::Object(len) = read_marker(raw)? {
+                        object_iterate(raw, len, |field, raw| {
+                            let c = Compression::read_raw(raw)?;
+                            entry_compress.push((field.to_string(), c));
+                            Ok(())
+                        })?;
+                    }
+                    else {
+                        return Err(Error::new(InvalidData, "`entries_compress` field doesn't contain an Object"));
+                    }
+                },
+               "field_type" | "max_fields" | "min_fields" | "req" | "opt" | "unknown_ok" => {
+                   object.update(field, raw, false, &mut types, &mut type_names)?;
+                },
+                "types" => {
+                    if let MarkerType::Object(len) = read_marker(raw)? {
+                        object_iterate(raw, len, |field, raw| {
+                            let v = Validator::read_validator(raw, false, &mut types, &mut type_names)?;
+                            if v == (types.len() - 1) {
+                                let v = types.pop();
+                                match field {
+                                    "Null" | "Bool" | "Int" | "Str" | "F32" | "F64" | "Bin" |
+                                    "Array" | "Obj" | "Hash" | "Ident" | "Lock" | "Time" | "Multi" => (),
+                                    _ => {
+                                        if let Some(index) = type_names.get(field) {
+                                            types[*index] = v.unwrap();
+                                        }
+                                    }
+                                }
+                            }
+                            Ok(())
+                        })?;
+                    }
+                    else {
+                        return Err(Error::new(InvalidData, "`entries` field doesn't contain an Object"));
+                    }
+                }
+                _ => {
+                    return Err(Error::new(InvalidData, "Unrecognized field in schema document"));
+                }
+            }
+            Ok(())
+        })?;
+
+        let hash = Hash::new(raw_for_hash);
+        Ok(Schema {
+            hash,
+            object,
+            entries,
+            types,
+            compressor: zstd_safe::create_cctx(),
+            decompressor: zstd_safe::create_dctx(),
+            doc_compress,
+            entry_compress,
+        })
+    }
+
+    pub fn hash(&self) -> &Hash {
+        &self.hash
+    }
+
+    pub fn encode_doc(&self, _doc: &Document, _buf: &mut Vec<u8>) -> io::Result<()> {
+        Err(Error::new(Other, "Document doesn't use this schema"))
+    }
+
+    /// Validates a document against this schema. Does not check the schema field itself.
+    pub fn validate_doc(&self, doc: &mut &[u8]) -> io::Result<()> {
+        let mut checklist = ValidatorChecklist::new();
+        self.object.validate("", doc, &self.types, &mut checklist, true).and(Ok(()))
+    }
+
+    /// Validates a given entry against this schema.
+    pub fn validate_entry(&self, entry: &str, doc: &mut &[u8]) -> io::Result<ValidatorChecklist> {
+        let mut checklist = ValidatorChecklist::new();
+        let v = self.entries.binary_search_by(|x| x.0.as_str().cmp(entry));
+        if v.is_err() { return Err(Error::new(InvalidData, "Entry field type doesn't exist in schema")); }
+        let v = self.entries[v.unwrap()].1;
+        self.types[v].validate("", doc, &self.types, 0, &mut checklist)?;
+        Ok(checklist)
+    }
+
+    /// Validates a document against a specific Hash Validator. Should be used in conjunction with 
+    /// a ValidatorChecklist returned from `validate_entry` to confirm that all documents referenced in an 
+    /// entry meet the schema's criteria.
+    pub fn validate_checklist_item(&self, index: usize, doc: &mut &[u8]) -> io::Result<()> {
+        if let Validator::Hash(ref v) = self.types[index] {
+            // Extract schema. Also verifies we are dealing with an Object (an actual document)
+            let doc_schema = extract_schema_hash(&doc.clone())?;
+            // Check against acceptable schemas
+            if v.schema_required() {
+                if let Some(hash) = doc_schema {
+                    if !v.schema_in_set(&hash) {
+                        return Err(Error::new(InvalidData, "Document uses unrecognized schema"));
+                    }
+                }
+                else {
+                    return Err(Error::new(InvalidData, "Document doesn't have schema, but needs one"));
+                }
+            }
+            if let Some(link) = v.link() {
+                let mut checklist = ValidatorChecklist::new();
+                if let Validator::Object(ref v) = self.types[link] {
+                    v.validate("", doc, &self.types, &mut checklist, true).and(Ok(()))
+                }
+                else {
+                    Err(Error::new(Other, "Can't validate a document against a non-object validator"))
+                }
+            }
+            else {
+                Ok(())
+            }
+        }
+        else {
+            Err(Error::new(Other, "Can't validate against non-hash validator"))
+        }
+
+    }
+}
 
 enum Compression {
     NoCompress,
     Compress(i32),
     DictCompress((zstd_safe::CDict<'static>, zstd_safe::DDict<'static>))
+}
+
+impl std::default::Default for Compression {
+    fn default() -> Self {
+        Compression::Compress(zstd_safe::CLEVEL_DEFAULT)
+    }
 }
 
 impl Compression {
@@ -169,10 +352,10 @@ impl Compression {
     }
 
     // Decompress raw data, after it has been stripped of the `CompressType` byte and the header if 
-    // present, which consists of the leading object tag and schema hash field-value pair. max_size 
-    // should reflect if the header has already been read. The intent is that the caller of this 
-    // function needed to vet the schema hash anyway, and verify that an Entry didn't start with 
-    // the Compressed flag, and that a Document didn't start with the CompressedNoSchema flag.
+    // present, which consists of the leading object tag and schema hash field-value pair.
+    // The intent is that the caller of this function needed to vet the schema hash anyway, and 
+    // verify that an Entry didn't start with the Compressed flag, and that a Document didn't start 
+    // with the CompressedNoSchema flag.
     fn decompress(
         &mut self,
         decompressor: &mut zstd_safe::DCtx,
@@ -243,221 +426,3 @@ impl Compression {
     }
 
 }
-
-
-/// Struct holding the validation portions of a schema. Can be used for validation of a document or 
-/// entry.
-pub struct Schema {
-    hash: Hash,
-    object: ValidObj,
-    entries: Vec<(String, usize)>,
-    types: Vec<Validator>,
-    doc_encode: Option<zstd_safe::CCtx<'static>>,
-    doc_decode: Option<zstd_safe::DCtx<'static>>,
-    entry_encode: Vec<(String, Option<zstd_safe::CCtx<'static>>)>,
-    entry_decode: Vec<(String, Option<zstd_safe::DCtx<'static>>)>,
-}
-
-impl Schema {
-    pub fn from_raw(raw: &mut &[u8]) -> io::Result<Schema> {
-        let raw_for_hash: &[u8] = raw;
-        let mut entries = Vec::new();
-        let mut types = Vec::with_capacity(2);
-        let mut type_names = HashMap::new();
-        let mut object = ValidObj::new(true); // Documents can always be queried, hence "true"
-        let doc_encode = Some(zstd_safe::create_cctx());
-        let doc_decode = Some(zstd_safe::create_dctx());
-        let mut entry_encode = Vec::new();
-        let mut entry_decode = Vec::new();
-        types.push(Validator::Invalid);
-        types.push(Validator::Valid);
-
-        let num_fields = match read_marker(raw)? {
-            MarkerType::Object(len) => len,
-            _ => return Err(Error::new(InvalidData, "Schema wasn't an object")),
-        };
-        object_iterate(raw, num_fields, |field, raw| {
-            match field {
-                "" => {
-                    read_hash(raw).map_err(|_e| Error::new(InvalidData, "Schema's empty field didn't contain root Schema Hash"))?;
-                },
-                "doc_compress" => {
-                    if let MarkerType::Object(len) = read_marker(raw)? {
-                        let mut format = None;
-                        let mut setting_seen = false;
-                        let mut setting = false;
-                        let mut setting_bin = None;
-                        object_iterate(raw, len, |field, raw| {
-                            match field {
-                                "format" => {
-                                    format = Some(read_integer(raw)?);
-                                },
-                                "setting" => {
-                                    setting_seen = true;
-                                    match read_marker(raw)? {
-                                        MarkerType::Boolean(v) => {
-                                            setting = v;
-                                        },
-                                        MarkerType::Binary(len) => {
-                                            let v = read_raw_bin(raw, len)?;
-                                            setting = true;
-                                            setting_bin = Some(v.to_vec());
-                                        },
-                                        _ => {
-                                            return Err(Error::new(InvalidData,
-                                                    "`doc_compress`/`setting` field didn't contain boolean or binary data"));
-                                        }
-                                    }
-                                },
-                                _ => {
-                                    return Err(Error::new(InvalidData,
-                                            format!("`doc_compress` contains unrecognized field `{}`", field)));
-                                }
-                            }
-                            if format.is_none() && setting_bin.is_some() {
-                                return Err(Error::new(InvalidData,
-                                        "Compression specifies a binary setting, but not a format"));
-                            }
-                            Ok(())
-                        })?;
-                    }
-                }
-                "description" => {
-                    read_str(raw).map_err(|_e| Error::new(InvalidData, "`description` field didn't contain string"))?;
-                },
-                "name" => {
-                    read_str(raw).map_err(|_e| Error::new(InvalidData, "`name` field didn't contain string"))?;
-                },
-                "version" => {
-                    read_integer(raw).map_err(|_e| Error::new(InvalidData, "`name` field didn't contain integer"))?;
-                },
-                "entries" => {
-                    if let MarkerType::Object(len) = read_marker(raw)? {
-                        object_iterate(raw, len, |field, raw| {
-                            let v = Validator::read_validator(raw, false, &mut types, &mut type_names)?;
-                            entries.push((field.to_string(), v));
-                            entry_encode.push((field.to_string(), Some(zstd_safe::create_cctx())));
-                            entry_decode.push((field.to_string(), Some(zstd_safe::create_dctx())));
-                            Ok(())
-                        })?;
-                    }
-                    else {
-                        return Err(Error::new(InvalidData, "`entries` field doesn't contain an Object"));
-                    }
-                },
-                /*
-                "entries_compress" => {
-                    if let MarkerType::Object(len) = read_marker(raw)? {
-                        object_iterate(raw, len, |field, raw| {
-                            Err(Error::new(InvalidData "entries_compress not implemented yet"))
-                        }
-                },
-                */
-               "field_type" | "max_fields" | "min_fields" | "req" | "opt" | "unknown_ok" => {
-                   object.update(field, raw, false, &mut types, &mut type_names)?;
-                },
-                "types" => {
-                    if let MarkerType::Object(len) = read_marker(raw)? {
-                        object_iterate(raw, len, |field, raw| {
-                            let v = Validator::read_validator(raw, false, &mut types, &mut type_names)?;
-                            if v == (types.len() - 1) {
-                                let v = types.pop();
-                                match field {
-                                    "Null" | "Bool" | "Int" | "Str" | "F32" | "F64" | "Bin" |
-                                    "Array" | "Obj" | "Hash" | "Ident" | "Lock" | "Time" | "Multi" => (),
-                                    _ => {
-                                        if let Some(index) = type_names.get(field) {
-                                            types[*index] = v.unwrap();
-                                        }
-                                    }
-                                }
-                            }
-                            Ok(())
-                        })?;
-                    }
-                    else {
-                        return Err(Error::new(InvalidData, "`entries` field doesn't contain an Object"));
-                    }
-                }
-                _ => {
-                    return Err(Error::new(InvalidData, "Unrecognized field in schema document"));
-                }
-            }
-            Ok(())
-        })?;
-
-        let hash = Hash::new(raw_for_hash);
-        Ok(Schema {
-            hash,
-            object,
-            entries,
-            types,
-            doc_encode,
-            doc_decode,
-            entry_encode,
-            entry_decode,
-        })
-    }
-
-    pub fn hash(&self) -> &Hash {
-        &self.hash
-    }
-
-    pub fn encode_doc(&self, _doc: &Document, _buf: &mut Vec<u8>) -> io::Result<()> {
-        Err(Error::new(Other, "Document doesn't use this schema"))
-    }
-
-    /// Validates a document against this schema. Does not check the schema field itself.
-    pub fn validate_doc(&self, doc: &mut &[u8]) -> io::Result<()> {
-        let mut checklist = ValidatorChecklist::new();
-        self.object.validate("", doc, &self.types, &mut checklist, true).and(Ok(()))
-    }
-
-    /// Validates a given entry against this schema.
-    pub fn validate_entry(&self, entry: &str, doc: &mut &[u8]) -> io::Result<ValidatorChecklist> {
-        let mut checklist = ValidatorChecklist::new();
-        let v = self.entries.binary_search_by(|x| x.0.as_str().cmp(entry));
-        if v.is_err() { return Err(Error::new(InvalidData, "Entry field type doesn't exist in schema")); }
-        let v = self.entries[v.unwrap()].1;
-        self.types[v].validate("", doc, &self.types, 0, &mut checklist)?;
-        Ok(checklist)
-    }
-
-    /// Validates a document against a specific Hash Validator. Should be used in conjunction with 
-    /// a ValidatorChecklist returned from `validate_entry` to confirm that all documents referenced in an 
-    /// entry meet the schema's criteria.
-    pub fn validate_checklist_item(&self, index: usize, doc: &mut &[u8]) -> io::Result<()> {
-        if let Validator::Hash(ref v) = self.types[index] {
-            // Extract schema. Also verifies we are dealing with an Object (an actual document)
-            let doc_schema = extract_schema_hash(&doc.clone())?;
-            // Check against acceptable schemas
-            if v.schema_required() {
-                if let Some(hash) = doc_schema {
-                    if !v.schema_in_set(&hash) {
-                        return Err(Error::new(InvalidData, "Document uses unrecognized schema"));
-                    }
-                }
-                else {
-                    return Err(Error::new(InvalidData, "Document doesn't have schema, but needs one"));
-                }
-            }
-            if let Some(link) = v.link() {
-                let mut checklist = ValidatorChecklist::new();
-                if let Validator::Object(ref v) = self.types[link] {
-                    v.validate("", doc, &self.types, &mut checklist, true).and(Ok(()))
-                }
-                else {
-                    Err(Error::new(Other, "Can't validate a document against a non-object validator"))
-                }
-            }
-            else {
-                Ok(())
-            }
-        }
-        else {
-            Err(Error::new(Other, "Can't validate against non-hash validator"))
-        }
-
-    }
-}
-

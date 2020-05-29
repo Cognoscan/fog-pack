821c9c37cb4191d8861fa4a1e817791d0cdc8b08
Scott Teal <scott@teals.org>
2020-05-29 14:05:09 -0400

    Adding support for base58 encode/decode to Hashes

--- src/crypto/hash.rs
+++ src/crypto/hash.rs
@@ -13,6 +13,18 @@ const DEFAULT_HASH_VERSION: u8 = 1;
 const MIN_HASH_VERSION: u8 = 1;
 const MAX_HASH_VERSION: u8 = 1;
 
+const ALPHABET: &'static [u8] = b"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+const DIGIT_MAP: &'static [i8] = &[
+    -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+    -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+    -1, 0, 1, 2, 3, 4, 5, 6, 7, 8,-1,-1,-1,-1,-1,-1,
+    -1, 9,10,11,12,13,14,15,16,-1,17,18,19,20,21,-1,
+    22,23,24,25,26,27,28,29,30,31,32,-1,-1,-1,-1,-1,
+    -1,33,34,35,36,37,38,39,40,41,42,43,-1,44,45,46,
+    47,48,49,50,51,52,53,54,55,56,57,-1,-1,-1,-1,-1,
+];
+
 /// Crytographically secure hash of data. Can be signed by a FullKey. It is impractical to generate an 
 /// identical hash from different data.
 ///
@@ -70,8 +82,34 @@ impl cmp::PartialOrd for Hash {
 }
 
 impl fmt::Debug for Hash {
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "{} {{ version: {:?}, digest: {:x?} }}", stringify!(Hash), &self.version, &self.digest[..])
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {{ version: {:?}, digest: {:x?} }}", stringify!(Hash), &self.version, &self.digest[..])
+    }
+}
+
+impl fmt::Display for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.encode_base58())
+    }
+}
+
+impl fmt::LowerHex for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::LowerHex::fmt(&self.version, f)?;
+        for byte in self.digest.iter() {
+            write!(f, "{:x}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::UpperHex for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::UpperHex::fmt(&self.version, f)?;
+        for byte in self.digest.iter() {
+            write!(f, "{:X}", byte)?;
+        }
+        Ok(())
     }
 }
 
@@ -140,6 +178,85 @@ impl Hash {
         let mut hash = Hash {version, digest:[0;HASH_BYTES]};
         buf.read_exact(&mut hash.digest).map_err(CryptoError::Io)?;
         Ok(hash)
+    }
+
+    pub fn encode_base58(&self) -> String {
+        // Version 0 means just a leading zero and nothing else.
+        if self.version == 0 {
+            return String::from("1");
+        }
+
+        // We never have leading zeros because the version byte is 1 (or higher)
+        // Run through base58 encoding loop, from the IETF Base58 Encoding Scheme 
+        // (draft-msporny-base58-01)
+        let mut input = Vec::with_capacity(self.len());
+        self.encode(&mut input);
+
+        let size = (input.len() * 89500 + 65535) >> 16; // ceil(size * log2(256)/log2(58)), scaled by 2^16.
+        let mut buffer = vec![0u8; size];
+        let mut high = size-1;
+
+        // Repeated long division by 58, emitting the remainder of each division
+        for i in 0..input.len() {
+            let mut carry = input[i] as u32;
+            let mut j = size-1;
+            while j > high || carry != 0 {
+                carry |= 256 * buffer[j] as u32;
+                let rem = carry % 58;
+                let quot = carry / 58;
+                buffer[j] = rem as u8;
+                carry = quot;
+                if j > 0 { j-=1; }
+            }
+            high = j;
+        }
+
+        let mut result = String::new();
+        for j in (buffer.iter().take_while(|x| **x == 0).count())..size {
+            result.push(char::from(ALPHABET[buffer[j] as usize]));
+        }
+
+        result
+    }
+
+    pub fn decode_base58(s: &str) -> Result<Hash, CryptoError> {
+        if s.len() < 1 { return Err(CryptoError::BadFormat); }
+        // If we only have one character and it's '1', then it's the empty hash.
+        if (s.len() == 1) && (s.as_bytes()[0] == b'1') {
+            return Ok(Hash::new_empty());
+        }
+        // Prepare to perform mapping & multiplication to decode
+        let mut buffer = [0u8; (HASH_BYTES+1)];
+        for byte in s.bytes() {
+            // Decode the character to 0-57, with -1 for invalid characters
+            let carry  = *DIGIT_MAP
+                .get(byte as usize)
+                .ok_or(CryptoError::BadFormat)?;
+            if carry == -1 { return Err(CryptoError::BadFormat); }
+            let mut carry = carry as u32;
+
+            // Big integer multiplication
+            for byte in buffer.iter_mut().rev() {
+                let t = (*byte as u32) * 58 + carry;
+                carry = (t & 0xFF00) >> 8;
+                *byte = (t & 0xFF) as u8;
+            }
+
+            // Shouldn't occur unless the hash is longer than expected
+            if carry != 0 { return Err(CryptoError::BadFormat); }
+        }
+        // The below array initializer looks stupid, yes, but it's quite probably the fastest way 
+        // to do this with safe rust. Slices complain about bounds, and everything else I know of 
+        // requires unsafe. Feel free to replace this if there's something more compact.
+        Ok(Hash {
+            version: buffer[0],
+            digest: [
+                buffer[ 1], buffer[ 2], buffer[ 3], buffer[ 4], buffer[ 5], buffer[ 6], buffer[ 7], buffer[ 8], 
+                buffer[ 9], buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15], buffer[16], 
+                buffer[17], buffer[18], buffer[19], buffer[20], buffer[21], buffer[22], buffer[23], buffer[24], 
+                buffer[25], buffer[26], buffer[27], buffer[28], buffer[29], buffer[30], buffer[31], buffer[32], 
+            ]
+        })
     }
 }
 
@@ -244,5 +361,27 @@ mod tests {
         assert_eq!(h.version(), 0);
         assert_eq!(h.digest(), &digest[..]);
         enc_dec(h);
+    }
+
+    #[test]
+    fn base58() {
+        use rand::prelude::*;
+        let mut rng = rand::thread_rng();;
+
+        for _ in 0..1000 {
+            let mut v: Vec<u8> = Vec::with_capacity(32);
+            for _ in 0..32 {
+                v.push(rng.gen());
+            }
+            let h = Hash::new(&v[..]);
+            let b58 = h.encode_base58();
+            let h2 = Hash::decode_base58(&b58).unwrap();
+            let eq = h == h2;
+            if !eq {
+                println!("in:  {}", h);
+                println!("out: {}", h2);
+            }
+            assert!(eq);
+        }
     }
 }

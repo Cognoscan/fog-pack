2d7723270ab3282acd5a9a2438b17f2bf82bb663
Scott Teal <scott@teals.org>
2021-03-13 15:22:00 -0500

    more VecDocumentBuilder fixes

--- src/document.rs
+++ src/document.rs
@@ -162,6 +162,7 @@ where
 {
     iter: std::iter::Fuse<I>,
     done: bool,
+    total: usize,
     ser: FogSerializer,
     item_buf: Vec<u8>,
     schema: Option<Hash>,
@@ -178,6 +179,7 @@ where
         Self {
             iter: iter.fuse(),
             done: false,
+            total: 0,
             ser: FogSerializer::default(),
             item_buf: Vec::new(),
             schema: schema.cloned(),
@@ -212,19 +214,23 @@ where
 
         let mut prev_len = self.ser.buf.len();
         let mut array_len = !self.ser.buf.is_empty() as usize;
-        while self.ser.buf.len() < data_len {
+        while self.ser.buf.len() <= data_len {
             let item = if let Some(item) = self.iter.next() { item } else { break };
             prev_len = self.ser.buf.len();
             item.serialize(&mut self.ser)?;
             array_len +=1;
         }
 
+
         if !self.ser.buf.is_empty() {
             // If we have excess data, lop it off and hold it for later copying
-            if prev_len != self.ser.buf.len() {
+            if self.ser.buf.len() > data_len {
                 self.item_buf.extend_from_slice(&self.ser.buf[prev_len..]);
                 self.ser.buf.truncate(prev_len);
+                array_len -= 1;
             }
+            self.total += array_len;
+            println!("Collecting {} items into a document, total = {}", array_len, self.total);
             // Create the new document
             let doc = NewDocument::new_from(self.schema.as_ref(), |mut buf| {
                 serialize_elem(&mut buf, crate::element::Element::Array(array_len));
@@ -239,15 +245,20 @@ where
                 Some(ref signer) => doc.sign(signer)?,
                 None => doc,
             };
-            // Move any lopped off data back into the serializer
+            // Move any lopped off data back into the serializer. If we have no lopped off data, 
+            // then we are out of stuff to write and can terminate
+            self.ser.buf.clear();
             if !self.item_buf.is_empty() {
-                self.ser.buf.clear();
                 self.ser.buf.extend_from_slice(&self.item_buf);
                 self.item_buf.clear();
+            }
+            else {
+                self.done = true;
             }
             Ok(Some(doc))
         }
         else {
+            self.done = true;
             Ok(None)
         }
     }
@@ -460,7 +471,9 @@ impl Document {
 
 #[cfg(test)]
 mod test {
-    use fog_crypto::lock::DEFAULT_LOCK_VERSION;
+    use rand::Rng;
+    use std::mem;
+    use std::ops;
 
     use super::*;
 
@@ -630,4 +643,251 @@ mod test {
             len <= (MAX_DOC_SIZE>>1) && len > (MAX_DOC_SIZE>>2)
         }));
     }
+
+    #[test]
+    fn vec_document_encode_all() {
+        #[derive(Clone, Serialize)]
+        struct Example {
+            a: u32,
+            b: String,
+        }
+
+        let iter = std::iter::repeat(Example { a: 23456, b: "Ok".into()}).take(MAX_DOC_SIZE + 12);
+        let builder = VecDocumentBuilder::new(iter, None);
+        let docs = builder.collect::<Result<Vec<NewDocument>>>().unwrap();
+        assert!(docs.iter().take(docs.len()-1).all(|doc| {
+            let len = doc.0.buf.len();
+            len <= (MAX_DOC_SIZE>>1) && len > (MAX_DOC_SIZE>>2)
+        }));
+        assert!(!docs.last().unwrap().data().is_empty());
+    }
+
+
+    pub trait Generate {
+        fn generate<R: Rng>(rng: &mut R) -> Self;
+    }
+    
+    impl Generate for () {
+        fn generate<R: Rng>(_: &mut R) -> Self {
+        }
+    }
+    
+    impl Generate for bool {
+        fn generate<R: Rng>(rng: &mut R) -> Self {
+            rng.gen_bool(0.5)
+        }
+    }
+    
+    macro_rules! impl_generate {
+        ($ty:ty) => {
+            impl Generate for $ty {
+                fn generate<R: Rng>(rng: &mut R) -> Self {
+                    rng.gen()
+                }
+            }
+        }
+    }
+    
+    impl_generate!(u8);
+    impl_generate!(u16);
+    impl_generate!(u32);
+    impl_generate!(u64);
+    impl_generate!(u128);
+    impl_generate!(usize);
+    impl_generate!(i8);
+    impl_generate!(i16);
+    impl_generate!(i32);
+    impl_generate!(i64);
+    impl_generate!(i128);
+    impl_generate!(isize);
+    impl_generate!(f32);
+    impl_generate!(f64);
+    
+    macro_rules! impl_tuple {
+        () => {};
+        ($first:ident, $($rest:ident,)*) => {
+            impl<$first: Generate, $($rest: Generate,)*> Generate for ($first, $($rest,)*) {
+                fn generate<R: Rng>(rng: &mut R) -> Self {
+                    ($first::generate(rng), $($rest::generate(rng),)*)
+                }
+            }
+    
+            impl_tuple!($($rest,)*);
+        };
+    }
+    
+    impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,);
+    
+    macro_rules! impl_array {
+        () => {};
+        ($len:literal, $($rest:literal,)*) => {
+            impl<T: Generate> Generate for [T; $len] {
+                fn generate<R: Rng>(rng: &mut R) -> Self {
+                    let mut result = mem::MaybeUninit::<Self>::uninit();
+                    let result_ptr = result.as_mut_ptr().cast::<T>();
+                    #[allow(clippy::reversed_empty_ranges)]
+                    for i in 0..$len {
+                        unsafe {
+                            result_ptr.add(i).write(T::generate(rng));
+                        }
+                    }
+                    unsafe {
+                        result.assume_init()
+                    }
+                }
+            }
+    
+            impl_array!($($rest,)*);
+        }
+    }
+    
+    impl_array!(31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, );
+    
+    impl<T: Generate> Generate for Option<T> {
+        fn generate<R: Rng>(rng: &mut R) -> Self {
+            if rng.gen_bool(0.5) {
+                Some(T::generate(rng))
+            } else {
+                None
+            }
+        }
+    }
+    
+    pub fn generate_vec<R: Rng, T: Generate>(rng: &mut R, range: ops::Range<usize>) -> Vec<T> {
+        let len = rng.gen_range(range.start, range.end);
+        let mut result = Vec::with_capacity(len);
+        for _ in 0..len {
+            result.push(T::generate(rng));
+        }
+        result
+    }
+
+    #[derive(Clone, Copy, serde::Serialize, serde::Deserialize)]
+    pub struct Address {
+        pub x0: u8,
+        pub x1: u8,
+        pub x2: u8,
+        pub x3: u8,
+    }
+
+    impl Generate for Address {
+        fn generate<R: Rng>(rand: &mut R) -> Self {
+            Self {
+                x0: rand.gen(),
+                x1: rand.gen(),
+                x2: rand.gen(),
+                x3: rand.gen(),
+            }
+        }
+    }
+
+    #[derive(Clone, serde::Serialize, serde::Deserialize)]
+    pub struct Log {
+        pub address: Address,
+        pub identity: String,
+        pub userid: String,
+        pub date: String,
+        pub request: String,
+        pub code: u16,
+        pub size: u64,
+    }
+
+    impl Generate for Log {
+        fn generate<R: Rng>(rand: &mut R) -> Self {
+            const USERID: [&str; 9] = [
+                "-",
+                "alice",
+                "bob",
+                "carmen",
+                "david",
+                "eric",
+                "frank",
+                "george",
+                "harry",
+            ];
+            const MONTHS: [&str; 12] = [
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+            ];
+            const TIMEZONE: [&str; 25] = [
+                "-1200", "-1100", "-1000", "-0900", "-0800", "-0700", "-0600", "-0500", "-0400",
+                "-0300", "-0200", "-0100", "+0000", "+0100", "+0200", "+0300", "+0400", "+0500",
+                "+0600", "+0700", "+0800", "+0900", "+1000", "+1100", "+1200",
+            ];
+            let date = format!(
+                "{}/{}/{}:{}:{}:{} {}",
+                rand.gen_range(1, 29),
+                MONTHS[rand.gen_range(0,12)],
+                rand.gen_range(1970,2022),
+                rand.gen_range(0,24),
+                rand.gen_range(0,60),
+                rand.gen_range(0,60),
+                TIMEZONE[rand.gen_range(0,25)],
+            );
+            const CODES: [u16; 63] = [
+                100, 101, 102, 103,
+                200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
+                300, 301, 302, 303, 304, 305, 306, 307, 308,
+                400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416,
+                417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451,
+                500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511,
+            ];
+            const METHODS: [&str; 5] = ["GET", "POST", "PUT", "UPDATE", "DELETE"];
+            const ROUTES: [&str; 7] = [
+                "/favicon.ico",
+                "/css/index.css",
+                "/css/font-awsome.min.css",
+                "/img/logo-full.svg",
+                "/img/splash.jpg",
+                "/api/login",
+                "/api/logout",
+            ];
+            const PROTOCOLS: [&str; 4] = [
+                "HTTP/1.0",
+                "HTTP/1.1",
+                "HTTP/2",
+                "HTTP/3",
+            ];
+            let request = format!(
+                "{} {} {}",
+                METHODS[rand.gen_range(0,5)],
+                ROUTES[rand.gen_range(0,7)],
+                PROTOCOLS[rand.gen_range(0,4)],
+            );
+            Self {
+                address: Address::generate(rand),
+                identity: "-".into(),
+                userid: USERID[rand.gen_range(0,USERID.len())].into(),
+                date,
+                request,
+                code: CODES[rand.gen_range(0,CODES.len())],
+                size: rand.gen_range(0,100_000_000),
+            }
+        }
+    }
+
+    #[test]
+    fn logs_encode() {
+
+        // Generate a whole pile of log items
+        let mut rng = rand::thread_rng();
+        const LOGS: usize = 10_000;
+        let logs = generate_vec::<_, Log>(&mut rng, LOGS..LOGS + 1);
+
+        // Try to make them into documents
+        let builder = VecDocumentBuilder::new(logs.iter(), None);
+        let docs = builder.collect::<Result<Vec<NewDocument>>>().unwrap();
+        for (index, doc) in docs.iter().enumerate() {
+            let mut parser = crate::element::Parser::with_debug(doc.data(), "  ");
+            while let Some(x) = parser.next() {
+                x.unwrap();
+            }
+            println!("Doc #{}: \n{}", index, parser.get_debug().unwrap());
+        }
+        assert!(docs.iter().take(docs.len()-1).all(|doc| {
+            let len = doc.0.buf.len();
+            len <= (MAX_DOC_SIZE>>1) && len > (MAX_DOC_SIZE>>2)
+        }));
+    }
+
 }
